9e898d44bfc72addafba8044585d9904d16c0b9d
Matthew Cumbo <mattcumbo99@gmail.com>
2022-12-31 14:50:46 -0600

    Added customizer page w/ dependencies
    This new page will be the main form for the application.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomizerComponent } from './pages/customizer/customizer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'main', component: CustomizerComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
